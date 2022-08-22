e70046e6aaa83a57f312dc833adde508e915578e
Ramzan <mdramzanroni76@gmail.com>
2022-08-22 16:28:19 +0600

    Load dynamic data, API call useEffect integrate state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
@@ -11,7 +11,8 @@ function App() {
   // ];
   return (
     <div className="App">
-      <Counter></Counter>
+      <ExternalUsers></ExternalUsers>
+      {/* <Counter></Counter> */}
       {/* {
         names.map(name=><Person name={name.name} position={name.position}></Person>)
       } */}
@@ -19,24 +20,49 @@ function App() {
   );
 }
 
-function Counter() {
-  const [counter, setCounter] = useState(1);
-  const increase = () => {
-    const newCount = counter + 1;
-    setCounter(newCount);
-  }
-  const decrease = () => {
-    const newCount = counter - 1;
-    setCounter(newCount);
-  }
+function ExternalUsers() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, [])
   return (
     <div>
-      <p>Counter :{counter}</p>
-      <button onClick={increase}>Increase</button>
-      <button onClick={decrease}>Decrease</button>
+      <h1>External Users form API</h1>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
     </div>
   )
 }
+
+function User(props) {
+  return (
+    <div className='user'>
+      <h2>Name: {props.name}</h2>
+      <p>Email: { props.email}</p>
+    </div>
+  )
+}
+// function Counter() {
+//   const [counter, setCounter] = useState(1);
+//   const increase = () => {
+//     const newCount = counter + 1;
+//     setCounter(newCount);
+//   }
+//   const decrease = () => {
+//     const newCount = counter - 1;
+//     setCounter(newCount);
+//   }
+//   return (
+//     <div>
+//       <p>Counter :{counter}</p>
+//       <button onClick={increase}>Increase</button>
+//       <button onClick={decrease}>Decrease</button>
+//     </div>
+//   )
+// }
 // function Person(props) {
   
 //   return (
