fd482ad51d4e8284e43ffce646ae539c49331210
Ramzan <mdramzanroni76@gmail.com>
2022-08-22 16:15:47 +0600

    State, component state hook and set state method

--- src/App.js
+++ src/App.js
@@ -1,30 +1,50 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const names = [
-    { name: "Ramzan", position: "Job Holder" },
-    { name: "Ramzan", position: "Job Holder" },
-    { name: "Ramzan", position: "Job Holder" },
-    { name: "Ramzan", position: "Job Holder" },
-    { name: "Ramzan", position: "Job Holder" }
-  ];
+  // const names = [
+  //   { name: "Ramzan", position: "Job Holder" },
+  //   { name: "Ramzan", position: "Job Holder" },
+  //   { name: "Ramzan", position: "Job Holder" },
+  //   { name: "Ramzan", position: "Job Holder" },
+  //   { name: "Ramzan", position: "Job Holder" }
+  // ];
   return (
     <div className="App">
-      {
+      <Counter></Counter>
+      {/* {
         names.map(name=><Person name={name.name} position={name.position}></Person>)
-      }
+      } */}
     </div>
   );
 }
 
-function Person(props) {
-  
+function Counter() {
+  const [counter, setCounter] = useState(1);
+  const increase = () => {
+    const newCount = counter + 1;
+    setCounter(newCount);
+  }
+  const decrease = () => {
+    const newCount = counter - 1;
+    setCounter(newCount);
+  }
   return (
-    <div className="person">
-      <h2>Name : {props.name}</h2>
-      <p>Position: {props.position}</p>
+    <div>
+      <p>Counter :{counter}</p>
+      <button onClick={increase}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
     </div>
   )
 }
+// function Person(props) {
+  
+//   return (
+//     <div className="person">
+//       <h2>Name : {props.name}</h2>
+//       <p>Position: {props.position}</p>
+//     </div>
+//   )
+// }
 
 export default App;
