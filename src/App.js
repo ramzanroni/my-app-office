5e55de9122734cf4ed2987197a67dc1399c98294
Ramzan <mdramzanroni76@gmail.com>
2022-08-22 15:56:49 +0600

    Create multiple components from an array of objects

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const names = [
+    { name: "Ramzan", position: "Job Holder" },
+    { name: "Ramzan", position: "Job Holder" },
+    { name: "Ramzan", position: "Job Holder" },
+    { name: "Ramzan", position: "Job Holder" },
+    { name: "Ramzan", position: "Job Holder" }
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        names.map(name=><Person name={name.name} position={name.position}></Person>)
+      }
     </div>
   );
+}
+
+function Person(props) {
+  
+  return (
+    <div className="person">
+      <h2>Name : {props.name}</h2>
+      <p>Position: {props.position}</p>
+    </div>
+  )
 }
 
 export default App;
